5a315142edd99a59e2142e178fdd94fe5b0800b9
MEDDHabib <159964076+MEDDHabib@users.noreply.github.com>
2024-02-18 23:21:28 +0100

    Delete SocketClient.java

--- SocketClient.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
-
-class SocketClient {
-    public static void main(String argv[]) {
-        int port = 0;
-        String host = "";
-       
